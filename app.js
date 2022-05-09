062d111eb89d9a577494cdfe6cf06b3bd913c401
Tibedo <thibaudbaily@hotmail.com>
2022-05-09 14:44:03 +0200

    Ajout de la description + Ajouter les jours restants à la liste (encore trouver pour les heures et secondes)

--- app.js
+++ app.js
@@ -3,20 +3,28 @@
 const todoInput = document.querySelector("#todo-input");
 const todoButton = document.querySelector("#todo-button");
 const todoList = document.querySelector(".todo-list");
-//const dateInput = document.querySelector("#date-input")
+const dateInput = document.querySelector("#date-input")
 
 todoButton.addEventListener("click", addToDo);
 todoList.addEventListener("click", deleteAndCheck);
 
 // FUNCTIONS
 
+
+
 function addToDo(e) {
   e.preventDefault();
   const divTodo = document.createElement("div");
   divTodo.classList.add("divTodo");
 
-  const list = document.createElement("li");
-  list.innerText = todoInput.value//, dateInput.value;
+// Date
+ let now = new Date()
+ let todoDate = new Date(dateInput.valueAsDate)
+
+ //console.log((todoDate.getDay() - now.getDay()) / (1000 * 60 * 60 * 24) + ' jours restants')
+
+  const list = document.createElement("li");  // Ajouter jours restant à la liste
+  list.innerText = todoInput.value + " " + (todoDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24) + ' jours restants';
   list.classList.add("todo-item");
   divTodo.appendChild(list);
 
@@ -30,22 +38,24 @@ function addToDo(e) {
   deletedButton.classList.add("deleted-btn");
   divTodo.appendChild(deletedButton);
 
+
+// Boutton pour description
   const descriptionButton = document.createElement("button");
   descriptionButton.innerHTML = '<img src="description.png" class="description">';
   descriptionButton.classList.add("description-btn");
   divTodo.appendChild(descriptionButton);
 
+  descriptionButton.addEventListener("click", function() {
+    let theDescription = document.createElement("input")
+    theDescription.className = "modify"
+  theDescription.type = "text"
+  divTodo.appendChild(theDescription)
+  })
+
   todoList.appendChild(divTodo);
   todoInput.value = "";
- // dateInput.value = "";
-
- // console.log(dateInput.value)
-
-// let now = new Date()
-// let todoDate = new Date(dateInput)
-
-// console.log((todoDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24) + ' jours restants')
-
+  
+  
 }
 
 
