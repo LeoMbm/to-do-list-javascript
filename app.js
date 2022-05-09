2f7a335d1093396c2ed0fe527336ead3c0d6886e
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-09 16:50:59 +0200

    button to closed modal

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 // Declaration Variable
 
-const todoInput = document.querySelector("#todo-input");
+const todoDescription = document.querySelector("#todo-descriptions");
 const todoButton = document.querySelector("#todo-button");
+const modalContainer = document.querySelector(".modal-container");
+const addButton = document.querySelector("#add");
+const closeButton = document.querySelector("#close");
 const todoList = document.querySelector(".todo-list");
 const filterOptions = document.querySelector("#todo-select");
+const dateInput = document.querySelector("#start");
 
-todoButton.addEventListener("click", addToDo);
+// EVENT LISTENER
+
+todoButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalContainer.classList.add("show");
+});
+
+addButton.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
+
+addButton.addEventListener("click", addToDo);
+
+closeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalContainer.classList.remove("show");
+});
+
 todoList.addEventListener("click", deleteAndCheck);
+
 filterOptions.addEventListener("click", filterTodo);
 
 // FUNCTIONS
@@ -16,8 +38,21 @@ function addToDo(e) {
   const divTodo = document.createElement("div");
   divTodo.classList.add("divTodo");
 
-  const list = document.createElement("li");
-  list.innerText = todoInput.value;
+  // Date
+  let oneDay = 24 * 60 * 60 * 1000;
+  let now = new Date();
+  let todoDate = new Date(dateInput.valueAsDate);
+
+  //console.log((todoDate.getDay() - now.getDay()) / (1000 * 60 * 60 * 24) + ' jours restants')
+
+  const list = document.createElement("li"); // Ajouter jours restant à la liste
+  let remainingTime =
+    todoDescription.value +
+    " " +
+    Math.ceil(Math.abs((todoDate.getTime() - now.getTime()) / oneDay)) +
+    " Days left";
+  console.log(remainingTime);
+  list.innerText = remainingTime;
   list.classList.add("todo-item");
   divTodo.appendChild(list);
 
@@ -31,8 +66,22 @@ function addToDo(e) {
   deletedButton.classList.add("deleted-btn");
   divTodo.appendChild(deletedButton);
 
+  // Boutton pour description
+  const descriptionButton = document.createElement("button");
+  descriptionButton.innerHTML =
+    '<img src="description.png" class="description">';
+  descriptionButton.classList.add("description-btn");
+  divTodo.appendChild(descriptionButton);
+
+  descriptionButton.addEventListener("click", function () {
+    let theDescription = document.createElement("input");
+    theDescription.className = "modify";
+    theDescription.type = "text";
+    divTodo.appendChild(theDescription);
+  });
+
   todoList.appendChild(divTodo);
-  todoInput.value = "";
+  todoDescription.value = "";
 }
 
 function deleteAndCheck(e) {
@@ -62,18 +111,11 @@ function filterTodo(e) {
           todo.style.display = "none";
         }
         break;
-      case "Doing":
-        if (todo.classList.contains("doing")) {
-          todo.style.display = "flex";
-        } else {
-          todo.style.display = "none";
-        }
-        break;
       case "To Do":
-        if (todo.classList.contains("todo")) {
-          todo.style.display = "flex";
-        } else {
+        if (todo.classList.contains("completed")) {
           todo.style.display = "none";
+        } else {
+          todo.style.display = "flex";
         }
         break;
     }
